6c5f3ed22078cbd21c24f17476cdd3443181be9d
ArchanaVeeru <61723206+ArchanaVeeru@users.noreply.github.com>
2020-05-31 10:18:41 -0700

    executing with pom

--- Framework/src/test/java/com/qa/amazon/TestPages/HomepageTest.java
+++ Framework/src/test/java/com/qa/amazon/TestPages/HomepageTest.java
@@ -1,7 +1,9 @@
 package com.qa.amazon.TestPages;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import com.qa.amazon.Pages.HomePage;
 import com.qa.amazon.Pages.LoginPage;
--- Framework/src/test/java/com/qa/amazon/TestPages/LoginTest.java
+++ Framework/src/test/java/com/qa/amazon/TestPages/LoginTest.java
@@ -4,8 +4,12 @@ package com.qa.amazon.TestPages;
 
 import java.io.IOException;
 
+
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 
 import com.qa.amazon.Pages.LoginPage;
 import com.qa.amazon.TestBase.TestBase;
